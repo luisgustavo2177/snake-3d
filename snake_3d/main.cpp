08d29319885d6d62b8e1a6145b7632a9365871fa
luisgustavo2177 <luisgustavolg2177@gmail.com>
2024-01-20 13:43:41 -0300

    Atualização simples 1

--- snake_3d/main.cpp
+++ snake_3d/main.cpp
@@ -6,27 +6,41 @@
 		   Luis Gustavo da Silva Sousa
     Data: 04/02/2024
  */
-#include <GL/gl.h>
-#include <GL/glut.h>
-#include <stdlib.h>
 
+//Bibliotecas usadas
+#ifdef __APPLE__
+    #define GL_SILENCE_DEPRECATION
+    #include <GLUT/glut.h>
+    #include <OpenGL/gl.h>
+    #include <OpenGL/glu.h>
+#else
+    #include <GL/glut.h>
+    #include <GL/gl.h>
+    #include <GL/glu.h>
+#endif
+
+//Declaração das funções
 void display_callback();
 
-int 
-main(int argc, char *argv[])
-{
-	glutCreateWindow("SNAKE 3D");
+void init(){
+	glClearColor(1.0, 0.5, 0.0,  1.0); //(R, G, B, alpha);
+}
+
+//Função Principal
+int main(int argc, char *argv[]){
+	glutCreateWindow("SNAKE 3D - OpenGL");//Nome do jogo
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    // glutInitWindowPosition(10,10);
+    glutInitWindowSize(1280, 720);//Tamanho da janela
+    glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-
     glutDisplayFunc(display_callback);
+    init();
     glutMainLoop();
-
     return EXIT_SUCCESS;
 }
 
+//Funções usadas
 void display_callback(){
+	glClear(GL_COLOR_BUFFER_BIT);
 	glutSwapBuffers();
 }
