4094f82e5276570ad5dd4f26bbd5876131bc1926
Heitor Moura <65419405+heitor-am@users.noreply.github.com>
2024-01-28 15:40:00 -0300

    melhorando geração da parede

--- snake_3d/main.cpp
+++ snake_3d/main.cpp
@@ -32,33 +32,55 @@
 #define ESC 27
 #define SPACE 32
 
-// Matriz de posições das paredes
-GLint wall_positions[][2] = {
-    // Superior
-	{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}, {9, 0}, {10, 0}, {11, 0}, {12, 0}, {13, 0}, {14, 0}, {15, 0}, {16, 0}, {17, 0}, {18, 0}, {19, 0}, {20, 0}, {21, 0}, {22, 0}, {23, 0}, {24, 0}, {25, 0}, {26, 0}, {27, 0}, {28, 0}, {29, 0}, {30, 0}, {31, 0}, {32, 0}, {33, 0}, {34, 0}, {35, 0}, {36, 0}, {37, 0}, {38, 0}, {39, 0}, {40, 0}, {41, 0}, {42, 0}, {43, 0}, {44, 0}, {45, 0}, {46, 0}, {47, 0}, {48, 0}, {49, 0}, {50, 0}, {51, 0}, {52, 0}, {53, 0}, {54, 0}, {55, 0}, {56, 0}, {57, 0}, {58, 0}, {59, 0}, {60, 0}, {61, 0}, {62, 0}, {63, 0}, {64, 0}, {87, 0}, {88, 0}, {89, 0},
-    {90, 0}, {91, 0}, {92, 0}, {93, 0}, {94, 0}, {95, 0}, {96, 0}, {97, 0}, {98, 0}, {99, 0}, {100, 0}, {101, 0}, {102, 0}, {103, 0}, {104, 0}, {105, 0}, {106, 0}, {107, 0}, {108, 0}, {109, 0}, {110, 0}, {111, 0}, {112, 0}, {113, 0}, {114, 0}, {115, 0}, {116, 0}, {117, 0}, {118, 0}, {119, 0}, {120, 0}, {121, 0}, {122, 0}, {123, 0}, {124, 0}, {125, 0}, {126, 0}, {127, 0}, {128, 0}, {129, 0}, {130, 0}, {131, 0}, {132, 0}, {133, 0}, {134, 0}, {135, 0}, {136, 0}, {137, 0}, {138, 0}, {139, 0}, {140, 0}, {141, 0}, {142, 0}, {143, 0}, {144, 0}, {145, 0}, {146, 0}, {147, 0}, {148, 0}, {149, 0}, {150, 0},
-	// Inferior
-    {0, 150}, {1, 150}, {2, 150}, {3, 150}, {4, 150}, {5, 150}, {6, 150}, {7, 150}, {8, 150}, {9, 150}, {10, 150}, {11, 150}, {12, 150}, {13, 150}, {14, 150}, {15, 150}, {16, 150}, {17, 150}, {18, 150}, {19, 150}, {20, 150}, {21, 150}, {22, 150}, {23, 150}, {24, 150}, {25, 150}, {26, 150}, {27, 150}, {28, 150}, {29, 150}, {30, 150}, {31, 150}, {32, 150}, {33, 150}, {34, 150}, {35, 150}, {36, 150}, {37, 150}, {38, 150}, {39, 150}, {40, 150}, {41, 150}, {42, 150}, {43, 150}, {44, 150}, {45, 150}, {46, 150}, {47, 150}, {48, 150}, {49, 150}, {50, 150}, {51, 150}, {52, 150}, {53, 150}, {54, 150}, {55, 150}, {56, 150}, {57, 150}, {58, 150}, {59, 150}, {60, 150}, {61, 150}, {62, 150}, {63, 150}, {64, 150}, {87, 150}, {88, 150},
-    {89, 150}, {90, 150}, {91, 150}, {92, 150}, {93, 150}, {94, 150}, {95, 150}, {96, 150}, {97, 150}, {98, 150}, {99, 150}, {100, 150}, {101, 150}, {102, 150}, {103, 150}, {104, 150}, {105, 150}, {106, 150}, {107, 150}, {108, 150}, {109, 150}, {110, 150}, {111, 150}, {112, 150}, {113, 150}, {114, 150}, {115, 150}, {116, 150}, {117, 150}, {118, 150}, {119, 150}, {120, 150}, {121, 150}, {122, 150}, {123, 150}, {124, 150}, {125, 150}, {126, 150}, {127, 150}, {128, 150}, {129, 150}, {130, 150}, {131, 150}, {132, 150}, {133, 150}, {134, 150}, {135, 150}, {136, 150}, {137, 150}, {138, 150}, {139, 150}, {140, 150}, {141, 150}, {142, 150}, {143, 150}, {144, 150}, {145, 150}, {146, 150}, {147, 150}, {148, 150}, {149, 150}, {150, 150},
-    // Esquerdo
-    {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7}, {0, 8}, {0, 9}, {0, 10}, {0, 11}, {0, 12}, {0, 13}, {0, 14}, {0, 15}, {0, 16}, {0, 17}, {0, 18}, {0, 19}, {0, 20}, {0, 21}, {0, 22}, {0, 23}, {0, 24}, {0, 25}, {0, 26}, {0, 27}, {0, 28}, {0, 29}, {0, 30}, {0, 31}, {0, 32}, {0, 33}, {0, 34}, {0, 35}, {0, 36}, {0, 37}, {0, 38}, {0, 39}, {0, 40}, {0, 41}, {0, 42}, {0, 43}, {0, 44}, {0, 45}, {0, 46}, {0, 47}, {0, 48}, {0, 49}, {0, 50}, {0, 51}, {0, 52}, {0, 53}, {0, 54}, {0, 55}, {0, 56}, {0, 57}, {0, 58}, {0, 59}, {0, 60}, {0, 61}, {0, 62}, {0, 63}, {0, 64}, {0, 65}, {0, 87}, {0, 88}, {0, 89},
-    {0, 90}, {0, 91}, {0, 92}, {0, 93}, {0, 94}, {0, 95}, {0, 96}, {0, 97}, {0, 98}, {0, 99}, {0, 100}, {0, 101}, {0, 102}, {0, 103}, {0, 104}, {0, 105}, {0, 106}, {0, 107}, {0, 108}, {0, 109}, {0, 110}, {0, 111}, {0, 112}, {0, 113}, {0, 114}, {0, 115}, {0, 116}, {0, 117}, {0, 118}, {0, 119}, {0, 120}, {0, 121}, {0, 122}, {0, 123}, {0, 124}, {0, 125}, {0, 126}, {0, 127}, {0, 128}, {0, 129}, {0, 130}, {0, 131}, {0, 132}, {0, 133}, {0, 134}, {0, 135}, {0, 136}, {0, 137}, {0, 138}, {0, 139}, {0, 140}, {0, 141}, {0, 142}, {0, 143}, {0, 144}, {0, 145}, {0, 146}, {0, 147}, {0, 148}, {0, 149}, {0, 150},
-    // Direito
-    {150, 1}, {150, 2}, {150, 3}, {150, 4}, {150, 5}, {150, 6}, {150, 7}, {150, 8}, {150, 9}, {150, 10}, {150, 11}, {150, 12}, {150, 13}, {150, 14}, {150, 15}, {150, 16}, {150, 17}, {150, 18}, {150, 19}, {150, 20}, {150, 21}, {150, 22}, {150, 23}, {150, 24}, {150, 25}, {150, 26}, {150, 27}, {150, 28}, {150, 29}, {150, 30}, {150, 31}, {150, 32}, {150, 33}, {150, 34}, {150, 35}, {150, 36}, {150, 37}, {150, 38}, {150, 39}, {150, 40}, {150, 41}, {150, 42}, {150, 43}, {150, 44}, {150, 45}, {150, 46}, {150, 47}, {150, 48}, {150, 49}, {150, 50}, {150, 51}, {150, 52}, {150, 53}, {150, 54}, {150, 55}, {150, 56}, {150, 57}, {150, 58}, {150, 59}, {150, 60}, {150, 61}, {150, 62}, {150, 63}, {150, 64}, {150, 87}, {150, 88}, {150, 89},
-    {150, 90}, {150, 91}, {150, 92}, {150, 93}, {150, 94}, {150, 95}, {150, 96}, {150, 97}, {150, 98}, {150, 99}, {150, 100}, {150, 101}, {150, 102}, {150, 103}, {150, 104}, {150, 105}, {150, 106}, {150, 107}, {150, 108}, {150, 109}, {150, 110}, {150, 111}, {150, 112}, {150, 113}, {150, 114}, {150, 115}, {150, 116}, {150, 117}, {150, 118}, {150, 119}, {150, 120}, {150, 121}, {150, 122}, {150, 123}, {150, 124}, {150, 125}, {150, 126}, {150, 127}, {150, 128}, {150, 129}, {150, 130}, {150, 131}, {150, 132}, {150, 133}, {150, 134}, {150, 135}, {150, 136}, {150, 137}, {150, 138}, {150, 139}, {150, 140}, {150, 141}, {150, 142}, {150, 143}, {150, 144}, {150, 145}, {150, 146}, {150, 147}, {150, 148}, {150, 149}, {150, 150}
-};
+// Defina um par para armazenar as coordenadas x e z
+typedef std::pair<int, int> Position;
+
+// Crie um vetor para armazenar as posições das paredes
+std::vector<Position> wall_positions;
+
+// Função para adicionar uma parede
+void add_wall(int x, int z) {
+    wall_positions.push_back({x, z});
+}
+
+// Função para inicializar as paredes
+void initialize_walls() {
+    // Esquerda
+    for (int i = 0; i <= 150; ++i) {
+        if (i >= 65 && i <= 85) continue; // Buraco
+        add_wall(i, 0);
+    }
+
+    // Direita
+    for (int i = 0; i <= 150; ++i) {
+        if (i >= 65 && i <= 85) continue;
+        add_wall(i, 150);
+    }
+ 
+    // Cima
+    for (int i = 0; i <= 150; ++i) {
+        if (i >= 65 && i <= 85) continue;
+        add_wall(0, i);
+    }
+
+    // Baixo
+    for (int i = 0; i <= 150; ++i) {
+        if (i >= 65 && i <= 85) continue;
+        add_wall(150, i);
+    }
+}
 
 // Variáveis de controle
 GLint lvl = 1;
 GLint points = 0;
-GLint size  = 0;
+GLint size = 0;
 GLbyte game_over = true;
 GLbyte enable_light = true;
 
 // Variáveis da cobra
 GLint body_pos[2][100] = {{}};
-GLint _x = 5;
-GLint _z = 10;
+GLint _x = 10;
+GLint _z = 15;
 GLint _oldX[2] = {};
 GLint _oldZ[2] = {};
 GLbyte direction = 0;
@@ -68,10 +90,10 @@ GLint _bx = 0;
 GLint _bz = 0;
 
 // Variáveis da janela
-GLint _Giw = 0;
-GLint _Gih = 0;
-GLint _Gfw = 150;
-GLint _Gfh = 150;
+GLint _iw = 0;
+GLint _ih = 0;
+GLint _fw = 150;
+GLint _fh = 150;
 
 // Variáveis para o ângulo da câmera
 static GLfloat view_rotx = 45.0F ;
@@ -97,6 +119,7 @@ int main(int argc, char** argv) {
     glutInitWindowSize(800,550);
     glutInitWindowPosition(80,80);
     glutCreateWindow("Snake Game 3D");
+    initialize_walls(); // Inicializa as paredes antes de iniciar o jogo
     init();
     glutSpecialFunc(special);
     glutKeyboardFunc(keyboard);
@@ -164,8 +187,8 @@ void manipulate_view_angle() {
 
 // Função para resetar o jogo
 void reset() {
-    _x = 5;
-    _z = 10;
+    _x = 10;
+    _z = 15;
     direction = 0;
     lvl = 1;
     points = 0;
@@ -240,7 +263,7 @@ void draw_food() {
     manipulate_view_angle();
     glTranslatef(_bx, -10.0, _bz);
     glColor3f(1, 1, 1);
-    glScalef(0.5, 0.5, 0.5);
+    glScalef(0.4, 0.4, 0.4);
     glutSolidSphere(7, 20, 20);
     glPopMatrix();
 }
@@ -273,12 +296,12 @@ void new_food() {
     bool food_on_wall;
     do {
         food_on_wall = false;
-        _bx = random_number(_Gfw - _Giw, _Giw + 10);
-        _bz = random_number(_Gfh - _Gih, _Gih + 10);
+        _bx = random_number(_fw - _iw, _iw + 10);
+        _bz = random_number(_fh - _ih, _ih + 10);
 
         // Checa se a comida não está em cima de alguma parede
-        for (int i = 0; i < sizeof(wall_positions) / sizeof(wall_positions[0]); i++) {
-            if (wall_positions[i][0] == _bx && wall_positions[i][1] == _bz) {
+        for (size_t i = 0; i < wall_positions.size(); i++) {
+            if (wall_positions[i].first == _bx && wall_positions[i].second == _bz) {
                 food_on_wall = true;
                 break;
             }
@@ -293,23 +316,21 @@ bool collision() {
     // Checa se a cobra colidiu com alguma parte do corpo
     for (i = 0; i < size; i++) {
         if ((body_pos[0][i] == _x && body_pos[1][i] == _z) ||
-        // ((body_pos[0][i] >= _x) && (body_pos[0][i] <= _x + 5) && (body_pos[1][i] >= _z) && (body_pos[1][i] <= _z + 5)) ||
-        // ((body_pos[0][i] <= _x) && (body_pos[0][i] >= _x - 5) && (body_pos[1][i] <= _z) && (body_pos[1][i] >= _z - 5)) ||
-        // ((body_pos[0][i] <= _x) && (body_pos[0][i] >= _x - 5) && (body_pos[1][i] >= _z) && (body_pos[1][i] <= _z + 5)) ||
+        ((body_pos[0][i] >= _x) && (body_pos[0][i] <= _x + 5) && (body_pos[1][i] >= _z) && (body_pos[1][i] <= _z + 5)) ||
+        ((body_pos[0][i] <= _x) && (body_pos[0][i] >= _x - 5) && (body_pos[1][i] <= _z) && (body_pos[1][i] >= _z - 5)) ||
+        ((body_pos[0][i] <= _x) && (body_pos[0][i] >= _x - 5) && (body_pos[1][i] >= _z) && (body_pos[1][i] <= _z + 5)) ||
         ((body_pos[0][i] >= _x) && (body_pos[0][i] <= _x + 5) && (body_pos[1][i] <= _z) && (body_pos[1][i] >= _z - 5)))
         return true;
     }
 
     // Checa se a cobra colidiu com alguma parede
-    for (i = 0; i < sizeof(wall_positions) / sizeof(wall_positions[0]); i++) {
-        if ((wall_positions[i][0] == _x && wall_positions[i][1] == _z) ||
-        ((wall_positions[i][0] >= _x) && (wall_positions[i][0] <= _x + 5) && (wall_positions[i][1] >= _z) && (wall_positions[i][1] <= _z + 5)) ||
-        // ((wall_positions[i][0] <= _x) && (wall_positions[i][0] >= _x - 5) && (wall_positions[i][1] <= _z) && (wall_positions[i][1] >= _z - 5)) ||
-        // ((wall_positions[i][0] <= _x) && (wall_positions[i][0] >= _x - 5) && (wall_positions[i][1] >= _z) && (wall_positions[i][1] <= _z + 5)) ||
-        ((wall_positions[i][0] >= _x) && (wall_positions[i][0] <= _x + 5) && (wall_positions[i][1] <= _z) && (wall_positions[i][1] >= _z - 5)))
-        return true;
+    for (size_t i = 0; i < wall_positions.size(); i++) {
+        if ((wall_positions[i].first == _x && wall_positions[i].second == _z) ||
+            ((wall_positions[i].first >= _x) && (wall_positions[i].first <= _x + 5) && (wall_positions[i].second >= _z) && (wall_positions[i].second <= _z + 5)) ||
+            ((wall_positions[i].first >= _x) && (wall_positions[i].first <= _x + 5) && (wall_positions[i].second <= _z) && (wall_positions[i].second >= _z - 5)))
+            return true;
     }
-
+    
     return false;
 }
 
@@ -324,22 +345,22 @@ void run(int value) {
           head_rotation = 90;
           _x += 6;
           // Checa se a cobra está indo para fora da tela, se sim, ela aparece do outro lado
-          if (_x > _Gfw-2) _x = _Giw-1;
+          if (_x > _fw-2) _x = _iw-1;
           break;
       case LEFT:
           head_rotation = -90;
           _x -= 6;
-         if (_x < 0) _x = _Gfw-2;
+         if (_x < 0) _x = _fw-2;
           break;
       case UP:
           head_rotation =0;
           _z += 6;
-          if (_z > _Gfh-2) _z = _Gih-1;
+          if (_z > _fh-2) _z = _ih-1;
           break;
       case DOWN:
           head_rotation = 180;
           _z -= 6;
-          if (_z < 2) _z = _Gfh - 2;
+          if (_z < 2) _z = _fh - 2;
           break;
     }
 
@@ -373,13 +394,13 @@ void run(int value) {
 
 // Adicione esta função para desenhar as paredes
 void draw_walls() {
-    for (int i = 0; i < sizeof(wall_positions) / sizeof(wall_positions[0]); ++i) {
+    for (size_t i = 0; i < wall_positions.size(); ++i) {
         glPushMatrix();
         manipulate_view_angle();
-        glTranslatef(wall_positions[i][0], -10.0, wall_positions[i][1]);
-        glColor3f(0.2, 0.2, 0.2); // Cor da parede
-        glScalef(0.5, 0.5, 0.5);  // Ajuste conforme necessário
-        glutSolidCube(7);         // Use glutSolidCube ou outro objeto para representar a parede
+        glTranslatef(wall_positions[i].first, -10.0, wall_positions[i].second);
+        glColor3f(0.2, 0.2, 0.2);
+        glScalef(0.5, 0.5, 0.5);
+        glutSolidCube(7);
         glPopMatrix();
     }
 }
@@ -411,19 +432,19 @@ void display(void) {
 void special(int key, int x, int y) {
     switch(key) {
     case GLUT_KEY_RIGHT:
-        if(direction != LEFT)
+        if (direction != LEFT)
             direction = RIGHT;
         break;
     case GLUT_KEY_LEFT:
-        if(direction != RIGHT)
+        if (direction != RIGHT)
             direction = LEFT;
         break;
     case GLUT_KEY_UP:
-        if(direction != UP)
+        if (direction != UP)
             direction = DOWN;
         break;
     case GLUT_KEY_DOWN:
-        if(direction != DOWN)
+        if (direction != DOWN)
             direction = UP;
         break;
     }
@@ -432,32 +453,24 @@ void special(int key, int x, int y) {
 // Função para capturar as teclas do teclado
 void keyboard(unsigned char key, int x, int y) { 
     switch (key) {
-    case SPACE:
+    case SPACE: // Iniciar/Reiniciar o jogo
         reset();
         glutPostRedisplay();
         break;
-	case 'W':
-    case 'w':
-        if (direction != UP)
-            direction = DOWN;
+	case 'W': case 'w':
+        if (direction != UP) direction = DOWN;
         break;
-    case 'S':
-    case 's':
-        if (direction != DOWN)
-            direction = UP;
+    case 'S': case 's':
+        if (direction != DOWN) direction = UP;
         break;
-    case 'A':
-    case 'a':
-        if (direction != RIGHT)
-            direction = LEFT;
+    case 'A': case 'a':
+        if (direction != RIGHT) direction = LEFT;
         break;
-    case 'D':
-    case 'd':
-        if (direction != LEFT)
-            direction = RIGHT;
+    case 'D': case 'd':
+        if (direction != LEFT) direction = RIGHT;
         break;
-    case ESC:
-        exit(0); // ESC para sair
+    case ESC: // Sair do jogo
+        exit(0);
         break;
     default:
         break;
