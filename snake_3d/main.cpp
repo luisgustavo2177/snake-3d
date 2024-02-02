de5422e1601503047f4ae7005f81b78a77593466
luisgustavo2177 <luisgustavolg2177@gmail.com>
2024-02-02 19:49:41 -0300

    Criação de telas, opção de pause, add de músicas, inicio da lógica de texturas

--- snake_3d/main.cpp
+++ snake_3d/main.cpp
@@ -24,6 +24,84 @@
 #include <windows.h>
 #include <vector>
 
+//Biblioteca para uso de som
+#include <mmsystem.h>
+
+//Biblioteca para criação das telas 
+#include "glut_text.h"
+
+// Configurar o som
+typedef struct {
+    const char* nome_arquivo;  // nome do arquivo de som
+    const char* ext;         // extensão(wav, mp4, mp3...)
+} music;
+
+// music sons_de_fundo = {"./music_de_fundo/classicMusicSnakeGame.wav", "wav"};
+// music sons_de_fundo = {"./music_de_fundo/Written-In-The-Stars-ft.wav", "wav"};
+music sons_de_fundo = {"./music_de_fundo/1.wav", "wav"};
+
+void playSound(){
+    PlaySound(sons_de_fundo.nome_arquivo, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+}
+
+void loadSound(){
+    PlaySound(sons_de_fundo.nome_arquivo, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP | SND_NOSTOP);
+    PlaySound(NULL, NULL, SND_ASYNC | SND_FILENAME | SND_LOOP | SND_NOSTOP | SND_PURGE);
+    PlaySound(sons_de_fundo.nome_arquivo, NULL, SND_FILENAME | SND_ASYNC | SND_LOOP | SND_NOSTOP | SND_NOWAIT);
+}
+
+//Configura as texturas
+typedef struct {////////////////////////////////////////////////////////////
+    int width;
+    int height;
+    int channels;
+    unsigned char* data;
+} Image;
+
+Image* load_image(const char* filename);
+void free_image(Image* image);
+
+// Armazena o ID da textura gerada pelo OpenGL
+GLuint textureID;////////////////////////////////////////////////////////////
+GLuint textureID1;
+GLuint textureID2;
+
+void lerImagem(const char* filename, int textura) {//////////////////////
+    Image* image = load_image(filename);
+
+    if (image) {
+    	if(textura == 1){
+			glGenTextures(1, &textureID);
+        	glBindTexture(GL_TEXTURE_2D, textureID);	
+		}else if(textura == 2){
+			glGenTextures(1, &textureID1);
+        	glBindTexture(GL_TEXTURE_2D, textureID1);
+		}else{
+			glGenTextures(1, &textureID2);
+        	glBindTexture(GL_TEXTURE_2D, textureID2);
+		}
+
+        // Configura os parï¿½metros da textura
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        // Carrega os dados da imagem na textura
+        GLenum format = (image->channels == 3) ? GL_RGB : GL_RGBA;
+        glTexImage2D(GL_TEXTURE_2D, 0, format, image->width, image->height, 0, format, GL_UNSIGNED_BYTE, image->data);
+
+        // Gera mipmaps para a textura
+        gluBuild2DMipmaps(GL_TEXTURE_2D, format, image->width, image->height, format, GL_UNSIGNED_BYTE, image->data);
+
+        // Desvincula a textura
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        // Libera a memï¿½ria da imagem
+        free_image(image);
+    }
+}
+
 // Definição de constantes
 #define UP 1
 #define DOWN 2
@@ -76,6 +154,7 @@ GLint points = 0;
 GLint size = 0;
 GLbyte game_over = true;
 GLbyte enable_light = true;
+GLbyte paused = false;
 
 // Variáveis da cobra
 GLint body_pos[2][100] = {{}};
@@ -112,6 +191,24 @@ void keyboard (unsigned char key, int x, int y);
 void run(int value);
 void new_food();
 
+// Função para desenhar a tela de boas-vindas
+bool game_started = false;//Verifica se deve mostrar a tela de Bem-vindo ou Game Over
+void welcome_screen_game_over_screen() {
+	if(!game_started){
+	    glColor3f(0.0, 0.0, 0.0); // Cor do texto (VERDE)
+	    draw_text_bitmap(35, 20, "BEM-VINDO");
+	    draw_text_bitmap(0, 10, "Este eh o jogo Snake-3D em OpenGL");
+	    draw_text_bitmap(-5, 0, "Para iniciar o jogo aperte a tecla espaco");
+	}
+    else if (game_over){
+		glColor3f(1.0, 0.0, 0.0); // Cor do texto (vermelho)
+	    draw_text_bitmap(35, 20, "GAME OVER");
+	    draw_text_bitmap(25, 10, "Pontuacao Final: " + to_string(points) + " ");
+	    draw_text_bitmap(0, 0, "Pressione ESPACO para reiniciar");
+	}
+}
+
+
 // Função principal
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
@@ -121,8 +218,18 @@ int main(int argc, char** argv) {
     glutCreateWindow("Snake Game 3D");
     initialize_walls(); // Inicializa as paredes antes de iniciar o jogo
     init();
+    
+	loadSound(); // carrega o som
+    playSound(); // inicia o som
+    
     glutSpecialFunc(special);
     glutKeyboardFunc(keyboard);
+    
+	glEnable(GL_DEPTH_TEST);
+	lerImagem("./texturas/snake.bmp", 1);
+	lerImagem("./texturas/grass_mine.bmp", 2);
+	lerImagem("./texturas/paredes.bmp", 3);
+    
     glutDisplayFunc(display);
     glutReshapeFunc(resize);
     new_food();
@@ -152,7 +259,7 @@ void init_light() {
 // Configurações iniciais
 void init(void) {
     glEnable(GL_DEPTH_TEST);
-    glClearColor(0.4f, 0.8f, 1.0f, 1.0f); // Muda a cor de fundo para azul céu
+    glClearColor(0.0, 1.0, 0.0, 1.0f); // Muda a cor de fundo para verde
     
     if (enable_light) {
         glEnable(GL_COLOR_MATERIAL); // Habilita cor
@@ -169,13 +276,6 @@ void resize (int w, int h) {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0, (double)w / (double)h, 1, 800.0);
-}
-
-// Função para escrever na tela
-void write(char *string) {
-    while(*string) {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, *string++);
-    }
 }
 
 // Função para manipular o ângulo da câmera
@@ -200,16 +300,6 @@ void reset() {
     head_rotation = 90.0F ;
 }
 
-// Função para desenhar a tela de boas-vindas
-void welcome_screen() {
-    glRasterPos2f(-40, 20);
-    write("========= BEM-VINDO =========");
-    glRasterPos2f(-40, 10);
-    write("========= Este é o jogo Snake-3D em OpenGL =========");
-    glRasterPos2f(-40, 0);
-    write("========= Para iniciar o jogo aperte a tecla espaco =========");
-}
-
 // Função para desenhar a cobra
 void draw_snake() {
     int  i;
@@ -217,14 +307,15 @@ void draw_snake() {
     // Desenha a cabeça da cobra e o plano
     glPushMatrix();
     manipulate_view_angle();
-        // Desenha o plano que a cobra vai correr
-        glPushMatrix();
-        //glColor3f(1, 0.7, 0.2);
-        glColor3f(0.0, 0.6, 0.2);
-        glTranslatef(75.0, -16.0, 75.0);
-        glScalef(155, 5.0, 155);
-        glutSolidCube(1);
-        glPopMatrix();
+
+    // Desenha o plano que a cobra vai correr
+    glPushMatrix();
+    glColor3f(0.0, 0.6, 0.2);
+    glTranslatef(75.0, -16.0, 75.0);
+    glScalef(155, 5.0, 155);
+	glutSolidCube(1);
+	glPopMatrix();
+	
 
     // Desenha a cabeça da cobra
     glColor3f(1, 0, 0); // Cor da cabeça da cobra (vermelho)
@@ -268,18 +359,15 @@ void draw_food() {
     glPopMatrix();
 }
 
-// Função para desenhar o status do jogo
+// Função para desenhar o status do jogo na tela
 void game_status() {
     char tmp_str[40];
-    glColor3f(0.8, 0.2, 0);
-    glRasterPos2f(5, 20);
-    sprintf(tmp_str, "Snake ");
-    write(tmp_str);
-    // Escreve o status do jogo na tela
+    glColor3f(1.0, 0.0, 0.0);
+    sprintf(tmp_str, "SNAKE 3D - GAME");
+    draw_text_bitmap(45, 20, tmp_str);
     glColor3f(0, 0, 0);
-    glRasterPos2f(5, 10);
-    sprintf(tmp_str, "Level: %d Points: %d", lvl, points);
-    write(tmp_str);
+    sprintf(tmp_str, "LEVEL: %d PONTOS: %d", lvl, points);
+    draw_text_bitmap(40, 10,tmp_str);
 }
 
 // Gerador de números aleatórios para a localização da comida que a cobra vai comer
@@ -336,60 +424,95 @@ bool collision() {
 
 // Função para movimentar a cobra
 void run(int value) {
-    int i;
-    _oldX[1] = _x;
-    _oldZ[1] = _z;
+    if (!paused) {
+        int i;
+        _oldX[1] = _x;
+        _oldZ[1] = _z;
 
-    switch (direction) {
-      case RIGHT:
-          head_rotation = 90;
-          _x += 6;
-          // Checa se a cobra está indo para fora da tela, se sim, ela aparece do outro lado
-          if (_x > _fw-2) _x = _iw-1;
-          break;
-      case LEFT:
-          head_rotation = -90;
-          _x -= 6;
-         if (_x < 0) _x = _fw-2;
-          break;
-      case UP:
-          head_rotation =0;
-          _z += 6;
-          if (_z > _fh-2) _z = _ih-1;
-          break;
-      case DOWN:
-          head_rotation = 180;
-          _z -= 6;
-          if (_z < 2) _z = _fh - 2;
-          break;
+        switch (direction) {
+        case RIGHT:
+            head_rotation = 90;
+            _x += 6;
+            // Checa se a cobra está indo para fora da tela, se sim, ela aparece do outro lado
+            if (_x > _fw - 2) _x = _iw - 1;
+            break;
+        case LEFT:
+            head_rotation = -90;
+            _x -= 6;
+            if (_x < 0) _x = _fw - 2;
+            break;
+        case UP:
+            head_rotation = 0;
+            _z += 6;
+            if (_z > _fh - 2) _z = _ih - 1;
+            break;
+        case DOWN:
+            head_rotation = 180;
+            _z -= 6;
+            if (_z < 2) _z = _fh - 2;
+            break;
+        }
+
+        // Checa se a cobra colidiu com alguma parede, se sim, o jogo acaba
+        if (collision()) game_over = true;
+
+        // Checa se a cobra comeu a comida, se sim, aumenta os pontos e o tamanho da cobra e cria uma nova comida
+        if ((_x == _bx && _z == _bz) ||
+            ((_x >= _bx) && (_x <= _bx + 4) && (_z >= _bz) && (_z <= _bz + 4)) ||
+            ((_x <= _bx) && (_x >= _bx - 4) && (_z <= _bz) && (_z >= _bz - 4)) ||
+            ((_x <= _bx) && (_x >= _bx - 4) && (_z >= _bz) && (_z <= _bz + 4)) ||
+            ((_x >= _bx) && (_x <= _bx + 4) && (_z <= _bz) && (_z >= _bz - 4))) {
+            points++;
+            if (points < 100) size++;
+            if ((points % 5) == 0 && lvl < 15) lvl++;
+            new_food();
+        }
+
+        for (i = 0; i < size; i++) { // Salva as posições das partes do corpo
+            _oldX[0] = _oldX[1];
+            _oldZ[0] = _oldZ[1];
+            _oldX[1] = body_pos[0][i];
+            _oldZ[1] = body_pos[1][i];
+            body_pos[0][i] = _oldX[0];
+            body_pos[1][i] = _oldZ[0];
+        }
+
+        // Configura o timer para a próxima iteração
+        glutTimerFunc(130, run, 0);
+    } else {
+        // Se o jogo estiver pausado, configura o timer para continuar verificando após um intervalo
+        glutTimerFunc(500, run, 0);
     }
+}
 
-    // Checa se a cobra colidiu com alguma parede, se sim, o jogo acaba
-    if (collision()) game_over = true;
+void drawTexturedCube(GLuint t) {
+    GLfloat vertices[][3] = {
+        {-1.0, -1.0, -1.0}, {1.0, -1.0, -1.0}, {1.0, 1.0, -1.0}, {-1.0, 1.0, -1.0},
+        {-1.0, -1.0, 1.0}, {1.0, -1.0, 1.0}, {1.0, 1.0, 1.0}, {-1.0, 1.0, 1.0}
+    };
 
-    // Checa se a cobra comeu a comida, se sim, aumenta os pontos e o tamanho da cobra e cria uma nova comida
-    if ((_x == _bx && _z == _bz) ||
-    ((_x >= _bx) && (_x <= _bx + 4) && (_z >= _bz) && (_z <= _bz + 4)) ||
-    ((_x <= _bx) && (_x >= _bx - 4) && (_z <= _bz) && (_z >= _bz - 4)) ||
-    ((_x <= _bx) && (_x >= _bx - 4) && (_z >= _bz) && (_z <= _bz + 4)) ||
-    ((_x >= _bx) && (_x <= _bx + 4) && (_z <= _bz) && (_z >= _bz - 4))) {
-        points++;
-        if (points < 100) size++;
-        if ((points % 5) == 0 && lvl < 15) lvl++;
-        new_food();
+    GLfloat texCoords[][2] = {
+        {0.0, 0.0}, {1.0, 0.0}, {1.0, 1.0}, {0.0, 1.0}
+    };
+
+    GLint faces[][4] = {
+        {0, 1, 2, 3}, {1, 5, 6, 2}, {5, 4, 7, 6}, {4, 0, 3, 7}, {3, 2, 6, 7}, {4, 5, 1, 0}
+    };
+
+    glBindTexture(GL_TEXTURE_2D, t);
+    glEnable(GL_TEXTURE_2D);
+
+    glBegin(GL_QUADS);
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            glTexCoord2fv(texCoords[j]);
+            glVertex3fv(vertices[faces[i][j]]);
+        }
     }
-
-    for (i = 0; i < size; i++) { // Salva as posições das partes do corpo
-        _oldX[0] = _oldX[1];
-        _oldZ[0] = _oldZ[1];
-        _oldX[1] = body_pos[0][i];
-        _oldZ[1] = body_pos[1][i];
-        body_pos[0][i] = _oldX[0];
-        body_pos[1][i] = _oldZ[0];
-    }
-
-    // Configura o timer para a velocidade da cobra
-    glutTimerFunc(130, run, 0);
+    glEnd();
+    
+    glDisable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 // Adicione esta função para desenhar as paredes
@@ -401,6 +524,7 @@ void draw_walls() {
         glColor3f(0.2, 0.2, 0.2);
         glScalef(0.5, 0.5, 0.5);
         glutSolidCube(7);
+        // drawTexturedCube(textureID2);
         glPopMatrix();
     }
 }
@@ -414,16 +538,15 @@ void display(void) {
 
     glTranslatef (-60.0, 40.0, zoom);
 
+    welcome_screen_game_over_screen();
+
     // Checa se o valor da flag "Game Over" não é verdadeiro, se for, continua o jogo
-    if (!game_over) {
+    if (!game_over && game_started) {
         game_status();
         draw_food();
         draw_snake();
         draw_walls();
-    } else {
-        welcome_screen();
     }
-
     glutPostRedisplay();
     glutSwapBuffers();
 }
@@ -455,6 +578,7 @@ void keyboard(unsigned char key, int x, int y) {
     switch (key) {
     case SPACE: // Iniciar/Reiniciar o jogo
         reset();
+        game_started = true;
         glutPostRedisplay();
         break;
 	case 'W': case 'w':
@@ -468,6 +592,9 @@ void keyboard(unsigned char key, int x, int y) {
         break;
     case 'D': case 'd':
         if (direction != LEFT) direction = RIGHT;
+        break;
+	case 'P': case 'p': // Pausar/Despausar o jogo
+        paused = !paused;
         break;
     case ESC: // Sair do jogo
         exit(0);
