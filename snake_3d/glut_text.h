12aff7ec683ebeb09c8f2360ab42714866aeb7f9
Heitor Moura <65419405+heitor-am@users.noreply.github.com>
2024-02-02 23:55:01 -0300

    alteração na biblioteca de texto

--- snake_3d/glut_text.h
+++ snake_3d/glut_text.h
@@ -1,9 +1,3 @@
-/*
- * Computacao Grafica
- * Codigo Exemplo: Biblioteca para escrever textos na janela GLUT/OpenGL
- * Autor: Prof. Laurindo de Sousa Britto Neto
- */
-
 #ifndef glut_text_h
 #define glut_text_h
 
@@ -17,57 +11,45 @@
 #include <string>
 #include <sstream>
 
-/*
- * Parametros padroes
- */
+/* Parâmetros padrões */
 #define SCALE 0.0007
 #define LINE_WIDTH 1.0
 GLvoid * font_bitmap = GLUT_BITMAP_HELVETICA_18;
 GLvoid * font_stroke = GLUT_STROKE_ROMAN;
 
-//cria o namespace my com a declaracao da assinatura da fucao to_string
-namespace my
-{
+// Cria o namespace my com a declaracao da assinatura da fucao to_string
+namespace my {
     template<typename T> std::string to_string(const T& value);
 }
 
-#if (__cplusplus >= 201103L) // Verifica a versao do compilador C++
-  using std::to_string; //usada a to_string do namespace std se a versao for maior ou igual ao C++11
+#if (__cplusplus >= 201103L) // Verifica a versão do compilador C++
+  using std::to_string; // usada a to_string do namespace std se a versão for maior ou igual ao C++11
 #else
-  using my::to_string;  //usada a to_string do namespace my em caso contrario
+  using my::to_string;  // usada a to_string do namespace my em caso contrario
 #endif
 
-/*
- * Converte numeros em std::string
- */
-template<typename T> std::string my::to_string(const T& value)
-{
+/* Converte números em std::string */
+template<typename T> std::string my::to_string(const T& value) {
     std::ostringstream s;
     s << value;
     return s.str();
 }
 
-/*
- * Desenha texto bitmap na posicao (x,y)
- */
-void draw_text_bitmap(float x, float y, std::string text, void * font = font_bitmap)
-{
+/* Desenha texto bitmap na posicao (x,y) */
+void draw_text_bitmap(float x, float y, std::string text, void * font = font_bitmap) {
     glRasterPos2f(x, y);
-    for(char * i = (char *)text.c_str(); *i; i++){ //for(char i : text){ //C++11
+    for(char * i = (char *)text.c_str(); *i; i++) { //for(char i : text){ //C++11
         glutBitmapCharacter(font, *i); //glutBitmapCharacter(font, i); //C++11
     }
 }
 
-/*
- * Desenha texto stroke na posicao (x,y)
- */
-void draw_text_stroke(float x, float y, std::string text, float scale = SCALE, float lineWidth = LINE_WIDTH, void * font = font_stroke)
-{
+/* Desenha texto stroke na posição (x,y) */
+void draw_text_stroke(float x, float y, std::string text, float scale = SCALE, float lineWidth = LINE_WIDTH, void * font = font_stroke) {
     glPushMatrix();
         glTranslatef(x, y, 0);
         glScalef(scale, scale, scale); // define o tamanho do fonte
         glLineWidth(lineWidth); // define a espessura da fonte
-        for(char * i = (char *)text.c_str(); *i; i++){
+        for(char * i = (char *)text.c_str(); *i; i++) {
             glutStrokeCharacter(font, *i);
         }
     glPopMatrix();
